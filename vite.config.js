d48469626680a0d2fa5103fffeca5017c17c00bb
hgjbkfgdfx <j_baby_39@yahoo.com>
2025-12-17 07:34:29 +0000

    Build production

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // REQUIRED for custom domain
   plugins: [react()],
   server: {
     allowedHosts: true
