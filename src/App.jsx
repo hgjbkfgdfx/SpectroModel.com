fbb7afe29994070f2e14de57e809213f02e9ce37
hgjbkfgdfx <j_baby_39@yahoo.com>
2025-12-18 01:07:29 +0000

    Save current work before switching to gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>✅ Welcome to SpectroModel</h1>
+    </div>
+  );
+}
+
+export default App;
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to SpectroModel</h1>
+    </div>
+  )
+}
+
+export default App
